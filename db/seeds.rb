77f7e25a63dd65da0559f053e7deff6d743398ad
tpr1992 <terence.pataneronan@gmail.com>
2019-07-26 09:12:12 -0400

    most recent version

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,25 @@
 
 
 Meme.destroy_all
+Favorite.destroy_all
+# User.destroy_all
 
 Meme.create(image_path: "https://vignette.wikia.nocookie.net/joke-battles/images/6/60/Ainsley-Harriott.jpg")
 Meme.create(image_path: 'https://imgflip.com/s/meme/Ancient-Aliens.jpg')
 Meme.create(image_path: 'http://i.imgur.com/DTZgsWl.jpg')
+
+User.create(name: 'Gino')
+User.create(name: 'Terence')
+User.create(name: 'Ludwig')
+User.create(name: 'Jen')
+User.create(name: 'Daniel')
+
+# Favorite.create(title: 'My second favorite', text_top: 'nah', text_bottom: 'ugh', meme_id: 1)
+# Favorite.create(title: 'My first favorite', text_top: 'Hello', text_bottom: 'Shit', user_id: 1, meme_id: 1)
+Favorite.create(title: 'Does this shit even work?', text_top: 'Probably', text_bottom: 'Not', user_id: 2, meme_id: 1)
+Favorite.create(title: 'Dumb Ass Meme', text_top: 'This meme', text_bottom: 'sucks', user_id: 2, meme_id: 2)
+Favorite.create(title: 'Ludwigs favorite', text_top: 'Im a', text_bottom: 'Tuxedo Cat', user_id: 3, meme_id: 3)
+Favorite.create(title: 'My first favorite', text_top: 'Hello', text_bottom: 'Shit', user_id: 3, meme_id: 3)
+Favorite.create(title: 'Jen is a good cook', text_top: 'Im hungry', text_bottom: 'for food', user_id: 4, meme_id: 3)
+Favorite.create(title: 'Last one', text_top: 'Fuck this', text_bottom: 'Shit', user_id: 5, meme_id: 1)
+Favorite.create(title: 'Last one', text_top: 'Lets get', text_bottom: 'This bread', user_id: 5, meme_id: 2)
